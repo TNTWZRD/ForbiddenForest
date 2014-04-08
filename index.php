d0bb795dc0c5564e2be646c7c9cb96f3c1d2b219
Daniel Jajliardo <tntwzrd.minecraft@gmail.com>
2014-04-08 16:54:23 -0400

    Main Webpage File

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>
+		Epic World
+	</title>
+</head>
+
+<body>
+
+<div class="titlebar">
+Epic World <b><i>~ The Forbidden Forest ~</i></b>. Hosted On: <a href="https://www.chessmenink.tk/">ChessmenInk</a>.
+ Written And Scripted By: <a href="mailto:danieljajliardo@gmail.com">Daniel Jajliardo</a>.
+</div>
+
+</body>	
+</html>
